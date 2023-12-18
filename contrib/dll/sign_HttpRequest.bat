abeedeea8cadcf81e62857d16019408540065cc1
Vlad Vissoultchev <wqweto@gmail.com>
2023-12-18 09:34:48 +0200

    Add binaries signing batch files

---
+++ contrib/dll/sign_HttpRequest.bat
@@ -0,0 +1,1 @@
+call C:\Work\BuildTools\Certificates\codesign.bat /d "Http Request Replacement" %~dp0HttpRequest.dll
---
+++ contrib/dll/sign_WinsockTls.bat
@@ -0,0 +1,1 @@
+call C:\Work\BuildTools\Certificates\codesign.bat /d "Winsock Control Replacement" %~dp0WinsockTls.ocx
